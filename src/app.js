45f06e70d0983f73a9e3d8221062280530f5c91e
wangxia/work <xiawang1024@icloud.com>
2018-10-10 16:46:03 +0800

    feat:tabBar配置

--- src/app.js
+++ src/app.js
@@ -13,6 +13,31 @@ class App extends Component {
 			navigationBarTitleText: 'WeChat',
 			navigationBarTextStyle: 'black',
 			navigationStyle: 'custom'
+		},
+		tabBar: {
+			color: '#999',
+			selectedColor: '#c20d23',
+			backgroundColor: '#efefef',
+			list: [
+				{
+					pagePath: 'pages/index/index',
+					iconPath: './icons/tab-00.png',
+					selectedIconPath: './icons/tab-01.png',
+					text: '发现'
+				},
+				{
+					pagePath: 'pages/index/index',
+					iconPath: './icons/tab-10.png',
+					selectedIconPath: './icons/tab-11.png',
+					text: '分类'
+				},
+				{
+					pagePath: 'pages/index/index',
+					iconPath: './icons/tab-20.png',
+					selectedIconPath: './icons/tab-21.png',
+					text: '我的'
+				}
+			]
 		}
 	};
 	componentWillMount() {
