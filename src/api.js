9d50ff9fb0fa91c6f868a6091a71c004cc603b04
wangxia/work <xiawang1024@icloud.com>
2019-01-10 11:16:45 +0800

    api 地址问题

--- src/api.js
+++ src/api.js
@@ -1,109 +1,104 @@
 const dataList = [
-  {
-    cid: '17',
-    description:
-      '贝多芬说，音乐是比一切智慧、一切哲学更高的启示。\n“天籁古典”音乐台，用古典音乐注入它她的内涵，选取不同时代不同体裁不同类型的作品，内容涵盖交响乐、协奏曲、奏鸣曲、清唱剧、交响诗、歌剧、舞剧音乐、组曲、序曲、夜曲、舞曲、器乐曲、抒情小品、古典民族民间音乐、现代经典创作、东西方民歌民谣、跨界音乐等。一支支经典的乐曲，铺阵通往心灵深处的长廊，为你展现博大精深的古典音乐。\n魅力回旋，释放经典。“天籁古典”音乐台，这里的古典音乐有温度，有情感。它是生活美学，也是生活哲学。你爱上它，它就会融入你的生命。\n“天籁古典”，为你而来。',
-    image: 'http://www.hndt.com/hnradio/17.jpg',
-    name: '天籁古典',
-    hotline: '',
-    interact: '1',
-    streams: ['http://60.205.147.84/live/5950a4a485027b68b9e8be8c.m3u8'],
-    video_streams: [''],
-    update_id: 0,
-    isprograms: 0,
-    live: '',
-    time: ''
-  },
-  {
-    cid: '18',
-    description:
-      '爵士音乐的俱乐部，随性的爵士乐，最新最快的娱乐资讯，陪你度过慵懒的午后时光。',
-    image: 'http://www.hndt.com/hnradio/18.jpg',
-    name: '爵士FM',
-    hotline: '',
-    interact: '1',
-    streams: ['http://60.205.147.84/live/589d4e6b2273b659ce4c3ea9.m3u8'],
-    video_streams: [''],
-    update_id: 0,
-    isprograms: 0,
-    live: '',
-    time: ''
-  },
-  {
-    cid: '19',
-    description:
-      '爱摇人群的时尚爬，奔放的电吉他SOLO、热血的青春鼓点、让你体验重金属嗓音带来声音快感。',
-    image: 'http://www.hndt.com/hnradio/19.jpg',
-    name: '摇滚天空台',
-    hotline: '',
-    interact: '1',
-    streams: ['http://60.205.147.84/live/589d4f122273b659ce4c3ecc.m3u8'],
-    video_streams: [''],
-    update_id: 0,
-    isprograms: 0,
-    live: '',
-    time: ''
-  },
-  {
-    cid: '20',
-    description:
-      '民谣爱好者的音乐盒，悠扬的歌声，动听的六弦琴给你别具一格的清新、畅快；',
-    image: 'http://www.hndt.com/hnradio/20.jpg',
-    name: '民谣音乐台',
-    hotline: '',
-    interact: '1',
-    streams: ['http://60.205.147.84/live/589d4ed02273b659ce4c3eb9.m3u8'],
-    video_streams: [''],
-    update_id: 0,
-    isprograms: 0,
-    live: '',
-    time: ''
-  },
-  {
-    cid: '21',
-    description:
-      '70后男性的火柴盒，它陪你阅读新鲜的资讯，欣赏经典的音乐，一起重温青春的记忆；',
-    image: 'http://www.hndt.com/hnradio/21.jpg',
-    name: '经典FM',
-    hotline: '',
-    interact: '1',
-    streams: ['http://r.yibo.so/live/55801ddeb3ee794c623ff452.m3u8'],
-    video_streams: [''],
-    update_id: 0,
-    isprograms: 0,
-    live: '',
-    time: ''
-  },
-  {
-    cid: '22',
-    description:
-      '80后男人的聚集地，我们一起选择高品质、懂生活的声音，我们一起享受上班下班寂寞的时光。',
-    image: 'http://www.hndt.com/hnradio/22.jpg',
-    name: '80后音悦台',
-    hotline: '',
-    interact: '1',
-    streams: ['http://60.205.147.84/live/5746ac3f934ff0e41b1e19b5.m3u8'],
-    video_streams: [''],
-    update_id: 0,
-    isprograms: 0,
-    live: '',
-    time: ''
-  },
-  {
-    cid: '23',
-    description:
-      '90后男生的生活玩伴，它陪你一起感受生命的朝气蓬勃、青春的逆反孤单；',
-    image: 'http://www.hndt.com/hnradio/23.jpg',
-    name: '潮流音悦台',
-    hotline: '',
-    interact: '1',
-    streams: ['http://60.205.147.84/live/5746ac6e934ff0e41b1e19b6.m3u8'],
-    video_streams: [''],
-    update_id: 0,
-    isprograms: 0,
-    live: '',
-    time: ''
-  }
+	{
+		cid: '17',
+		description:
+			'贝多芬说，音乐是比一切智慧、一切哲学更高的启示。\n“天籁古典”音乐台，用古典音乐注入它她的内涵，选取不同时代不同体裁不同类型的作品，内容涵盖交响乐、协奏曲、奏鸣曲、清唱剧、交响诗、歌剧、舞剧音乐、组曲、序曲、夜曲、舞曲、器乐曲、抒情小品、古典民族民间音乐、现代经典创作、东西方民歌民谣、跨界音乐等。一支支经典的乐曲，铺阵通往心灵深处的长廊，为你展现博大精深的古典音乐。\n魅力回旋，释放经典。“天籁古典”音乐台，这里的古典音乐有温度，有情感。它是生活美学，也是生活哲学。你爱上它，它就会融入你的生命。\n“天籁古典”，为你而来。',
+		image: 'http://www.hndt.com/hnradio/17.jpg',
+		name: '天籁古典',
+		hotline: '',
+		interact: '1',
+		streams: [ 'http://60.205.147.84/live/5950a4a485027b68b9e8be8c.m3u8' ],
+		// streams: [ 'http://stream.hndt.com/live/xinwen/playlist.m3u8' ],
+		video_streams: [ '' ],
+		update_id: 0,
+		isprograms: 0,
+		live: '',
+		time: ''
+	},
+	{
+		cid: '18',
+		description: '爵士音乐的俱乐部，随性的爵士乐，最新最快的娱乐资讯，陪你度过慵懒的午后时光。',
+		image: 'http://www.hndt.com/hnradio/18.jpg',
+		name: '爵士FM',
+		hotline: '',
+		interact: '1',
+		streams: [ 'http://60.205.147.84/live/589d4e6b2273b659ce4c3ea9.m3u8' ],
+		video_streams: [ '' ],
+		update_id: 0,
+		isprograms: 0,
+		live: '',
+		time: ''
+	},
+	{
+		cid: '19',
+		description: '爱摇人群的时尚爬，奔放的电吉他SOLO、热血的青春鼓点、让你体验重金属嗓音带来声音快感。',
+		image: 'http://www.hndt.com/hnradio/19.jpg',
+		name: '摇滚天空台',
+		hotline: '',
+		interact: '1',
+		streams: [ 'http://60.205.147.84/live/589d4f122273b659ce4c3ecc.m3u8' ],
+		video_streams: [ '' ],
+		update_id: 0,
+		isprograms: 0,
+		live: '',
+		time: ''
+	},
+	{
+		cid: '20',
+		description: '民谣爱好者的音乐盒，悠扬的歌声，动听的六弦琴给你别具一格的清新、畅快；',
+		image: 'http://www.hndt.com/hnradio/20.jpg',
+		name: '民谣音乐台',
+		hotline: '',
+		interact: '1',
+		streams: [ 'http://60.205.147.84/live/589d4ed02273b659ce4c3eb9.m3u8' ],
+		video_streams: [ '' ],
+		update_id: 0,
+		isprograms: 0,
+		live: '',
+		time: ''
+	},
+	{
+		cid: '21',
+		description: '70后男性的火柴盒，它陪你阅读新鲜的资讯，欣赏经典的音乐，一起重温青春的记忆；',
+		image: 'http://www.hndt.com/hnradio/21.jpg',
+		name: '经典FM',
+		hotline: '',
+		interact: '1',
+		streams: [ 'http://r.yibo.so/live/55801ddeb3ee794c623ff452.m3u8' ],
+		video_streams: [ '' ],
+		update_id: 0,
+		isprograms: 0,
+		live: '',
+		time: ''
+	},
+	{
+		cid: '22',
+		description: '80后男人的聚集地，我们一起选择高品质、懂生活的声音，我们一起享受上班下班寂寞的时光。',
+		image: 'http://www.hndt.com/hnradio/22.jpg',
+		name: '80后音悦台',
+		hotline: '',
+		interact: '1',
+		streams: [ 'http://60.205.147.84/live/5746ac3f934ff0e41b1e19b5.m3u8' ],
+		video_streams: [ '' ],
+		update_id: 0,
+		isprograms: 0,
+		live: '',
+		time: ''
+	},
+	{
+		cid: '23',
+		description: '90后男生的生活玩伴，它陪你一起感受生命的朝气蓬勃、青春的逆反孤单；',
+		image: 'http://www.hndt.com/hnradio/23.jpg',
+		name: '潮流音悦台',
+		hotline: '',
+		interact: '1',
+		streams: [ 'http://60.205.147.84/live/5746ac6e934ff0e41b1e19b6.m3u8' ],
+		video_streams: [ '' ],
+		update_id: 0,
+		isprograms: 0,
+		live: '',
+		time: ''
+	}
 ]
 
 export default dataList
--- src/components/player/index.js
+++ src/components/player/index.js
@@ -12,247 +12,226 @@ import { AddCount } from '../../api/index'
 const backgroundAudioManager = Taro.getBackgroundAudioManager()
 
 export default class Player extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isPlayInfo: {},
-      isPlay: true,
-      isShowInfo: false,
-      isShowList: false
-    }
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			isPlayInfo: {},
+			isPlay: true,
+			isShowInfo: false,
+			isShowList: false
+		}
+	}
 
-  componentWillMount() {}
+	componentWillMount() {}
 
-  componentDidMount() {
-    this.playEventListen()
-    let { cid } = this.props
-    // console.log('------------------------------------')
-    console.log(cid)
-    // console.log('------------------------------------')
+	componentDidMount() {
+		this.playEventListen()
+		let { cid } = this.props
+		// console.log('------------------------------------')
+		console.log(cid)
+		// console.log('------------------------------------')
 
-    this.selectPlay(cid)
-  }
+		this.selectPlay(cid)
+	}
 
-  componentWillUnmount() {}
+	componentWillUnmount() {}
 
-  componentDidShow() {}
+	componentDidShow() {}
 
-  componentDidHide() {}
+	componentDidHide() {}
 
-  playEventListen() {
-    backgroundAudioManager.onCanplay(() => {
-      Taro.hideLoading()
-    })
-    backgroundAudioManager.onPlay(() => {
-      Taro.hideLoading()
-    })
-    backgroundAudioManager.onWaiting(() => {
-      Taro.showLoading({
-        title: '切换中...'
-      })
-    })
-  }
-  playSwitch(e) {
-    e.preventDefault()
-    let { isPlay } = this.state
-    if (isPlay) {
-      isPlay = false
-      backgroundAudioManager.pause()
-    } else {
-      isPlay = true
-      backgroundAudioManager.play()
-    }
+	playEventListen() {
+		backgroundAudioManager.onCanplay(() => {
+			Taro.hideLoading()
+		})
+		backgroundAudioManager.onPlay(() => {
+			Taro.hideLoading()
+		})
+		backgroundAudioManager.onWaiting(() => {
+			Taro.showLoading({
+				title: '切换中...'
+			})
+		})
+	}
+	playSwitch(e) {
+		e.preventDefault()
+		let { isPlay } = this.state
+		if (isPlay) {
+			isPlay = false
+			backgroundAudioManager.pause()
+		} else {
+			isPlay = true
+			backgroundAudioManager.play()
+		}
 
-    this.setState({ isPlay })
-  }
-  onCloseInfo = () => {
-    this.setState({
-      isShowInfo: false
-    })
-  }
-  onOpenInfo = () => {
-    this.setState({
-      isShowInfo: true
-    })
-  }
-  onCloseList = () => {
-    this.setState({
-      isShowList: false
-    })
-  }
-  onOpenList = () => {
-    this.setState({
-      isShowList: true
-    })
-  }
-  selectPlay = cid => {
-    let selected = dataList.filter(item => item.cid == cid)
-    this.setPlaySrc(selected[0])
-    this.setState({
-      isPlayInfo: selected[0]
-    })
-  }
-  setPlaySrc = data => {
-    backgroundAudioManager.title = data.name
-    backgroundAudioManager.epname = data.name
-    backgroundAudioManager.coverImgUrl = data.image
-    backgroundAudioManager.protocol = 'hls'
-    backgroundAudioManager.src = data.streams[0]
-    this.playHistory(data.cid)
-    AddCount(parseInt(data.cid))
-    Taro.setNavigationBarTitle({
-      title: data.name
-    })
-  }
+		this.setState({ isPlay })
+	}
+	onCloseInfo = () => {
+		this.setState({
+			isShowInfo: false
+		})
+	}
+	onOpenInfo = () => {
+		this.setState({
+			isShowInfo: true
+		})
+	}
+	onCloseList = () => {
+		this.setState({
+			isShowList: false
+		})
+	}
+	onOpenList = () => {
+		this.setState({
+			isShowList: true
+		})
+	}
+	selectPlay = (cid) => {
+		let selected = dataList.filter((item) => item.cid == cid)
+		this.setPlaySrc(selected[0])
+		this.setState({
+			isPlayInfo: selected[0]
+		})
+	}
+	setPlaySrc = (data) => {
+		backgroundAudioManager.title = data.name
+		backgroundAudioManager.epname = data.name
+		backgroundAudioManager.coverImgUrl = data.image
+		backgroundAudioManager.protocol = 'hls'
+		backgroundAudioManager.src = data.streams[0]
+		this.playHistory(data.cid)
+		AddCount(parseInt(data.cid))
+		Taro.setNavigationBarTitle({
+			title: data.name
+		})
+	}
 
-  formatDate = () => {
-    let date = new Date()
+	formatDate = () => {
+		let date = new Date()
 
-    let year = date.getFullYear(),
-      month = date.getMonth() + 1, //月份是从0开始的
-      day = date.getDate(),
-      hour = date.getHours(),
-      min = date.getMinutes(),
-      sec = date.getSeconds()
-    min = min < 10 ? '0' + min : min
-    sec = sec < 10 ? '0' + sec : sec
-    let newTime =
-      year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec
-    return newTime
-  }
-  playHistory = cid => {
-    let list = []
-    let keyName = 'history_list'
-    let time = this.formatDate()
-    Taro.getStorage({
-      key: keyName
-    })
-      .then(res => {
-        list = res.data
-        list.push({ cid, time })
-        Taro.setStorage({ key: keyName, data: list })
-      })
-      .catch(err => {
-        list = []
-        Taro.setStorage({ key: keyName, data: list })
-        console.log(err)
-      })
-  }
-  onSwitchPlay = cid => {
-    this.selectPlay(cid)
-  }
-  seeIndexCid = () => {
-    let { isPlayInfo } = this.state
-    let indexCid = 0
-    dataList.forEach((item, index) => {
-      if (isPlayInfo.cid == item.cid) {
-        indexCid = index
-      } else {
-        return false
-      }
-    })
+		let year = date.getFullYear(),
+			month = date.getMonth() + 1, //月份是从0开始的
+			day = date.getDate(),
+			hour = date.getHours(),
+			min = date.getMinutes(),
+			sec = date.getSeconds()
+		min = min < 10 ? '0' + min : min
+		sec = sec < 10 ? '0' + sec : sec
+		let newTime = year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec
+		return newTime
+	}
+	playHistory = (cid) => {
+		let list = []
+		let keyName = 'history_list'
+		let time = this.formatDate()
+		Taro.getStorage({
+			key: keyName
+		})
+			.then((res) => {
+				list = res.data
+				list.push({ cid, time })
+				Taro.setStorage({ key: keyName, data: list })
+			})
+			.catch((err) => {
+				list = []
+				Taro.setStorage({ key: keyName, data: list })
+				console.log(err)
+			})
+	}
+	onSwitchPlay = (cid) => {
+		this.selectPlay(cid)
+	}
+	seeIndexCid = () => {
+		let { isPlayInfo } = this.state
+		let indexCid = 0
+		dataList.forEach((item, index) => {
+			if (isPlayInfo.cid == item.cid) {
+				indexCid = index
+			} else {
+				return false
+			}
+		})
 
-    return indexCid
-  }
-  clickNext = () => {
-    let len = dataList.length
-    let indexCid = this.seeIndexCid()
-    if (indexCid == len - 1) {
-      Taro.showToast({
-        title: '没有更多数据',
-        icon: 'none'
-      })
-    }
-    ++indexCid
-    let playInfo = dataList[indexCid]
-    this.selectPlay(playInfo.cid)
-  }
-  clickPrev = () => {
-    let indexCid = this.seeIndexCid()
-    if (indexCid == 0) {
-      Taro.showToast({
-        title: '没有更多数据',
-        icon: 'none'
-      })
-    }
-    --indexCid
-    let playInfo = dataList[indexCid]
-    this.selectPlay(playInfo.cid)
-  }
+		return indexCid
+	}
+	clickNext = () => {
+		let len = dataList.length
+		let indexCid = this.seeIndexCid()
+		if (indexCid == len - 1) {
+			Taro.showToast({
+				title: '没有更多数据',
+				icon: 'none'
+			})
+		}
+		++indexCid
+		let playInfo = dataList[indexCid]
+		this.selectPlay(playInfo.cid)
+	}
+	clickPrev = () => {
+		let indexCid = this.seeIndexCid()
+		if (indexCid == 0) {
+			Taro.showToast({
+				title: '没有更多数据',
+				icon: 'none'
+			})
+		}
+		--indexCid
+		let playInfo = dataList[indexCid]
+		this.selectPlay(playInfo.cid)
+	}
 
-  render() {
-    let { isPlay, isShowInfo, isShowList, isPlayInfo } = this.state
-    return (
-      <View className='player-box'>
-        <View className='bg-wrap'>
-          <Image className='logo-bg' src={isPlayInfo.image} mode='aspectFill' />
-        </View>
+	render() {
+		let { isPlay, isShowInfo, isShowList, isPlayInfo } = this.state
+		return (
+			<View className='player-box'>
+				<View className='bg-wrap'>
+					<Image className='logo-bg' src={isPlayInfo.image} mode='aspectFill' />
+				</View>
 
-        <View className='player-wrap'>
-          <View className={isPlay ? 'logo-wrap playing' : 'logo-wrap'}>
-            <Image className='logo-bg' src={require('./logo-bg.png')} />
-            <Image className='logo' src={isPlayInfo.image} />
-          </View>
-          <View className='slide-wrap'>
-            <Slider
-              step='1'
-              activeColor='#c20d23'
-              backgroundColor='#ddd'
-              blockSize='12'
-              value='30'
-              min='0'
-              max='100'
-              disabled
-            />
-          </View>
-        </View>
+				<View className='player-wrap'>
+					<View className={isPlay ? 'logo-wrap playing' : 'logo-wrap'}>
+						<Image className='logo-bg' src={require('./logo-bg.png')} />
+						<Image className='logo' src={isPlayInfo.image} />
+					</View>
+					<View className='slide-wrap'>
+						<Slider
+							step='1'
+							activeColor='#c20d23'
+							backgroundColor='#ddd'
+							blockSize='12'
+							value='30'
+							min='0'
+							max='100'
+							disabled
+						/>
+					</View>
+				</View>
 
-        <View className='ctrl-wrap'>
-          <View className='info' onClick={this.onOpenInfo}>
-            <Image className='icon-info' src={require('./icon-info.png')} />
-          </View>
-          <View className='btn-wrap'>
-            <Image
-              onClick={this.clickPrev}
-              className='icon-prev'
-              src={require('./icon-prev.png')}
-            />
-            <View className='icon-play-wrap' onClick={this.playSwitch}>
-              <Image
-                className='icon-play-pause'
-                src={
-                  isPlay
-                    ? require('./icon-pause.png')
-                    : require('./icon-play.png')
-                }
-              />
-            </View>
-            <Image
-              onClick={this.clickNext}
-              className='icon-next'
-              src={require('./icon-next.png')}
-            />
-          </View>
-          <View className='list' onClick={this.onOpenList}>
-            <Image className='icon-list' src={require('./icon-list.png')} />
-          </View>
-        </View>
-        {isShowInfo ? (
-          <PlayInfo onCloseInfo={this.onCloseInfo} playInfo={isPlayInfo} />
-        ) : (
-          ''
-        )}
-        {isShowList ? (
-          <PlayList
-            onCloseList={this.onCloseList}
-            onSwitchPlay={this.onSwitchPlay}
-            playCid={isPlayInfo.cid}
-          />
-        ) : (
-          ''
-        )}
-      </View>
-    )
-  }
+				<View className='ctrl-wrap'>
+					<View className='info' onClick={this.onOpenInfo}>
+						<Image className='icon-info' src={require('./icon-info.png')} />
+					</View>
+					<View className='btn-wrap'>
+						<Image onClick={this.clickPrev} className='icon-prev' src={require('./icon-prev.png')} />
+						<View className='icon-play-wrap' onClick={this.playSwitch}>
+							<Image
+								className='icon-play-pause'
+								src={isPlay ? require('./icon-pause.png') : require('./icon-play.png')}
+							/>
+						</View>
+						<Image onClick={this.clickNext} className='icon-next' src={require('./icon-next.png')} />
+					</View>
+					<View className='list' onClick={this.onOpenList}>
+						<Image className='icon-list' src={require('./icon-list.png')} />
+					</View>
+				</View>
+				{isShowInfo ? <PlayInfo onCloseInfo={this.onCloseInfo} playInfo={isPlayInfo} /> : ''}
+				{isShowList ? (
+					<PlayList onCloseList={this.onCloseList} onSwitchPlay={this.onSwitchPlay} playCid={isPlayInfo.cid} />
+				) : (
+					''
+				)}
+			</View>
+		)
+	}
 }
